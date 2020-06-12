27575726c5cb09f2f8dc7eb2283213e214174b5e
Seth Anderson <sdande02@gmail.com>
2020-06-12 05:55:33 -0700

    about to deploy

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
-const app = require('./app')
+const express = require('express');
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 
-const { PORT } = require('./config')
+const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/*', (req, res) => {
+  res.json({ ok: true });
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = { app };
