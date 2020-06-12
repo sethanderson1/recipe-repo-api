d3fad8051bc30ce8792fab880b2376c2458e47d8
Seth Anderson <sdande02@gmail.com>
2020-06-12 06:32:17 -0700

    adds CLIENT_ORIGIN for cors

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 3000
   }
   
--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
+
 const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 
 const app = express();
 app.use(morgan(morganSetting))
-
+// or just app.use(cors())
+app.use(
+  cors({
+      origin: CLIENT_ORIGIN
+  })
+);
 const PORT = process.env.PORT || 3000;
 
 app.get('/api/*', (req, res) => {
