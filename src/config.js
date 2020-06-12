21d49c8adc1d26d5f6373b37d6ee902217c6f302
Seth Anderson <sdande02@gmail.com>
2020-06-12 10:21:52 -0700

    adds config and postgrator-config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:p@localhost/recipe-repo',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin:p@localhost/recipe-repo-test',
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 3000
   }
   
