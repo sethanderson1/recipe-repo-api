e1bac3b28ac58bbb7b81ee054380d9bd1238fd6e
Seth Anderson <sdande02@gmail.com>
2020-06-16 11:35:04 -0700

    edited a seed

--- seeds/001.seed.users.sql
+++ seeds/001.seed.users.sql
@@ -1,8 +1,9 @@
 INSERT INTO users (id, user_name, password, date_created, date_modified)
 VALUES 
-(1,'user1@gmail.com', 'Password1!', '2020-03-22T16:28:32.615Z','2020-03-22T16:28:32.615Z'),
-(2,'user2@gmail.com', 'Password1!', '2020-03-22T16:28:32.615Z','2020-03-22T16:28:32.615Z'),
-(3,'user3@gmail.com', 'Password1!', '2020-03-22T16:28:32.615Z','2020-03-22T16:28:32.615Z');
+-- all passwords are 'Password1!'
+(1,'user1@gmail.com', '$2a$10$do9aqSHBEOX2mmidBFxxPOdknoul45m.TDoeQkdumVNrEkF2sn3KK', '2020-03-22T16:28:32.615Z','2020-03-22T16:28:32.615Z'),
+(2,'user2@gmail.com', '$2a$10$4AGfWWmypn45VHgsdyMOY.xymoQInQIwtzbFWeyvUvWVknpeSWwki', '2020-03-22T16:28:32.615Z','2020-03-22T16:28:32.615Z'),
+(3,'user3@gmail.com', '$2a$10$XrZZWlImZXIhAIf7b.8xqeDNK6kDU81An9lU.XzsuvgtR7hyG43fm', '2020-03-22T16:28:32.615Z','2020-03-22T16:28:32.615Z');
 
 SELECT * FROM users;
 
