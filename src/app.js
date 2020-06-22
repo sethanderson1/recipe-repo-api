db8b119ada9adb6bc6fb7b0047fcc796597bdc70
Seth Anderson <sdande02@gmail.com>
2020-06-22 16:53:32 -0700

    attempt 17 or so

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(cors())
 // );
 app.use(helmet())
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname,
     `/index.html`), function (err) {
