a319db8e104e3f01fff46f1707ffbb9af8996fae
Seth Anderson <sdande02@gmail.com>
2020-06-22 16:22:14 -0700

    12

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const db = knex({
 
 // app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/*', (req, res) => {
-  res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  res.sendfile(path.join(__dirname,'build/index.html'));
 })
 
 
