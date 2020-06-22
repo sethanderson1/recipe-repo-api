fcf6384ce84757a45469d3585bd2c16cfe4c0157
Seth Anderson <sdande02@gmail.com>
2020-06-22 16:17:16 -0700

    11

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ const db = knex({
 
 
 app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendfile(path.join(__dirname = 'client/build/index.html'));
 })
 
